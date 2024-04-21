4209c6123308cff67b2f14932289063842450467
apperedo <76085026+apperedo@users.noreply.github.com>
2024-04-21 16:47:34 -0400

    Working rows, columns and diagonal validations

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,53 @@ window.addEventListener('load',()=>{
     ];
 
     const boxes = document.querySelectorAll('.box');
+    
+    const isFull = () => {
+        const cp_boxes = [...game].flat(Infinity);
+        const isTotal = cp_boxes.every(box => box !== null);
+        return isTotal;
+    }
+    
+    const checkWinner = () => {
+        const isWinnerRowOne = [game[0][0], game[0][1], game[0][2]].every(item => item === game[0][0]);
+        const isWinnerRowTwo = [game[1][0], game[1][1], game[1][2]].every(item => item === game[1][0]);
+        const isWinnerRowThree = [game[2][0], game[2][1], game[2][2]].every(item => item === game[2][0]);
+        const isWinnerColOne = [game[0][0], game[1][0], game[2][0]].every(item => item === game[0][0]);
+        const isWinnerColTwo = [game[0][1], game[1][1], game[2][1]].every(item => item === game[0][1]);
+        const isWinnerColThree = [game[0][2], game[1][2], game[2][2]].every(item => item === game[0][2]);
+        const isWinnerDiagonalOne = [game[0][0], game[1][1], game[2][2]].every(item => item === game[0][0]);
+        const isWinnerDiagonalTwo = [game[0][2], game[1][1], game[2][0]].every(item => item === game[0][2]);
+        if (isWinnerRowOne || isWinnerColOne || isWinnerDiagonalOne) {
+            //alert(`Player ${game[0][0]} Wins`);
+            alert('1');
+            window.location.reload();
+            return;
+        }
+        if (isWinnerRowTwo) {
+            //alert(`Player ${game[1][0]} Wins`);
+            alert('2');
+            window.location.reload();
+            return;
+        }
+        if (isWinnerRowThree) {
+            //alert(`Player ${game[2][0]} Wins`);
+            alert('3');
+            window.location.reload();
+            return;
+        }
+        if (isWinnerColTwo) {
+            //alert(`Player ${game[0][1]} Wins`);
+            alert('4');
+            window.location.reload();
+            return;
+        }
+        if (isWinnerColThree || isWinnerDiagonalTwo) {
+            //alert(`Player ${game[0][2]} Wins`);
+            alert('5');
+            window.location.reload();
+            return;
+        }}
+
     boxes.forEach((box) => {
         box.addEventListener('click', () => {
           const row = box.getAttribute('data-row');
@@ -17,7 +64,10 @@ window.addEventListener('load',()=>{
           console.log(row,column);
           box.innerHTML=turn;
           turn = turn === playerOne ? playerTwo : playerOne;
-          console.log(game);
+          if (isFull()){
+            checkWinner();
+          }
+          
         });
     })
     
